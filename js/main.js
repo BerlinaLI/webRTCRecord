7ae9b67dd970964c8c2bb8e06fb9b6317165493a
berlina.li <berlina.li@akqa.com>
2019-06-16 18:55:21 +0800

    clean code

--- js/main.js
+++ js/main.js
@@ -1,45 +1,3 @@
-//file:///Users/berlina.li/art/record/index.html
-
-/*
-Copyright 2017 Google Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-'use strict';
-
-/* globals MediaRecorder */
-
-// This code is adapted from
-// https://rawgit.com/Miguelao/demos/master/mediarecorder.html
-
-/*
-Copyright 2017 Google Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-'use strict';
-
-/* globals MediaRecorder */
-
-// This code is adapted from
-// https://rawgit.com/Miguelao/demos/master/mediarecorder.html
-
 var mediaSource = new MediaSource();
 mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
 var mediaRecorder;
@@ -58,11 +16,16 @@ var constraints = {
   video: true
 };
 
+function handleSourceOpen(event) {
+  console.log('MediaSource opened');
+  sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+  console.log('Source buffer: ', sourceBuffer);
+}
+
 navigator.mediaDevices.getUserMedia(
   constraints
 ).then(
-  successCallback,
-  errorCallback
+  successCallback
 );
 
 function successCallback(stream) {
@@ -70,83 +33,22 @@ function successCallback(stream) {
   window.stream = stream;
   gumVideo.srcObject = stream;
 
-  startRecording();
-
-  setTimeout(function() {
-    stopRecording();
-    play();
-  }, 2500);
-}
-
-function errorCallback(error) {
-  console.log('navigator.getUserMedia error: ', error);
-}
-
-function handleSourceOpen(event) {
-  console.log('MediaSource opened');
-  sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
-  console.log('Source buffer: ', sourceBuffer);
-}
-
-
-// The nested try blocks will be simplified when Chrome 47 moves to Stable
-function startRecording() {
+  // start
   var options = {mimeType: 'video/webm', bitsPerSecond: 100000};
   recordedBlobs = [];
-  try {
-    mediaRecorder = new MediaRecorder(window.stream, options);
-  } catch (e0) {
-    console.log('Unable to create MediaRecorder with options Object: ', e0);
-    try {
-      options = {mimeType: 'video/webm,codecs=vp9', bitsPerSecond: 100000};
-      mediaRecorder = new MediaRecorder(window.stream, options);
-    } catch (e1) {
-      console.log('Unable to create MediaRecorder with options Object: ', e1);
-      try {
-        options = 'video/vp8'; // Chrome 47
-        mediaRecorder = new MediaRecorder(window.stream, options);
-      } catch (e2) {
-        alert('MediaRecorder is not supported by this browser.\n\n' +
-            'Try Firefox 29 or later, or Chrome 47 or later, with Enable experimental Web Platform features enabled from chrome://flags.');
-        console.error('Exception while creating MediaRecorder:', e2);
-        return;
-      }
-    }
-  }
-  // console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  mediaRecorder = new MediaRecorder(window.stream, options);
   mediaRecorder.ondataavailable = function(event) {
     if (event.data && event.data.size > 0) {
       recordedBlobs.push(event.data);
     }
   }
-  mediaRecorder.start(10); // collect 10ms of data
-  // console.log('MediaRecorder started', mediaRecorder);
-  console.log('start recording')
-}
+  mediaRecorder.start(10);
 
-function stopRecording() {
-  mediaRecorder.stop();
-  // console.log('Recorded Blobs: ', recordedBlobs);
-  recordedVideo.controls = false;
-  console.log('stop recording')
-}
-
-function play() {
-  var superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
-  recordedVideo.src = window.URL.createObjectURL(superBuffer);
-}
-
-function download() {
-  var blob = new Blob(recordedBlobs, {type: 'video/webm'});
-  var url = window.URL.createObjectURL(blob);
-  var a = document.createElement('a');
-  a.style.display = 'none';
-  a.href = url;
-  a.download = 'test.webm';
-  document.body.appendChild(a);
-  a.click();
   setTimeout(function() {
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  }, 100);
+    // stop
+    mediaRecorder.stop();
+    // play
+    var superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
+    recordedVideo.src = window.URL.createObjectURL(superBuffer);
+  }, 2500);
 }
